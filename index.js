7a58f3fa9bd64c09b4480ad4ca5061c3744e2431
Laurence Cadag <laurencecads1@gmail.com>
2023-05-24 23:54:35 +0800

    get all categories and populate

--- index.js
+++ index.js
@@ -1,50 +1,80 @@
 const itemList = document.getElementById("productList");
 const buttons = document.querySelectorAll(".btnAdd");
+const loader = document.querySelector(".loader-bg");
 const cart = document.getElementById("cart");
+const store = document.getElementById("store");
+const tabs = document.getElementById("categoryTabs");
 
 let productItems = [];
 let cartItems = [];
+let categories = [];
 
-fetch("https://fakestoreapi.com/products")
-  .then((res) => res.json())
-  .then((data) => {
-    productItems = data;
-    console.log("product items", productItems);
-    // checkCart();
-    productItems.forEach((item) => {
-      let card = document.createElement("div");
-      card.classList.add(
-        "flex",
-        "flex-col",
-        "box",
-        "relative",
-        "transition",
-        "ease-in-out",
-        "delay-120",
-        "shadow-lg"
-      );
-      // <div class='flex box flex-col relative box transition ease-in-out delay-120 shadow-lg'></div>
-      card.innerHTML = `
-          <img src="${item.image}" class='w-full h-full'>
-          <h4 class='text-center text-md'> ${item.title} </h4>
-        `;
-      const button = document.createElement("button");
-      button.classList.add("bg-red-700", "text-white");
-      button.textContent = `Add to Cart`;
-      button.onclick = () => {
-        // console.log(item); // The entire item object can be accessed here
-        cartItems.push(item); // The entire item object can be accessed here
-        console.log(item);
-        // shareViaEmail();
-        checkCart();
-      };
-      card.appendChild(button);
-      itemList.appendChild(card);
+async function getCategories () {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products/categories');
+    const data = await response.json();
+    categories = data;
+
+    tabs.innerHTML += `<button id="all" class="px-2 py-2 text-gray-800 shadow-lg uppercase"> All </button>`;
+    categories.forEach((category) => {
+      tabs.innerHTML += `<button  class="px-2 py-2 text-gray-800 shadow-lg uppercase" id="${category}"> ${category}</button>`
+    })
+  }catch (error) {
+    console.log(error);
+  }
+}
+
+async function fetchProducts() {
+  try {
+
+    getCategories();
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        loader.classList.add("active");
+        productItems = data;
+        productItems.forEach((item) => {
+          let card = document.createElement("div");
+          card.classList.add(
+            "flex",
+            "flex-col",
+            "box",
+            "relative",
+            "transition",
+            "ease-in-out",
+            "delay-120",
+            "shadow-lg",
+          );
+          card.innerHTML = `
+              <img src="${item.image}" class='product-item h-full'>
+              <h4 class='text-center text-md'> ${item.title.substring(0, 25) + "..."} </h4>
+            `;
+          const actionManager = document.createElement('div')
+          actionManager.classList.add('grid-by-2');
+          const viewBtn = document.createElement('button')
+          viewBtn.classList.add('border-2', "border-gray-500","text-neutral-700", "w-full", "px-2", "py-2", 'font-bold')
+          viewBtn.textContent = "View Product";
+          const button = document.createElement("button");
+          button.classList.add("bg-red-700", "text-white", "w-full", "px-2", "py-2", 'font-bold');
+          button.textContent = `Add to Cart`;
+
+          actionManager.appendChild(viewBtn)
+          actionManager.appendChild(button)
+          button.onclick = () => {
+          
+            cartItems.push(item); // The entire item object can be accessed here
+            checkCart();
+          };
+          card.appendChild(actionManager);
+          itemList.appendChild(card);
+          
+        });
     });
-  });
-
-
-// console.log(cartItems.length)
+  }catch (error) {
+    console.log(error);
+  }
+  loader.classList.remove('active')
+}
 
 function checkCart() {
   if (cartItems.length == 0) {
@@ -67,3 +97,10 @@ function checkCart() {
     cart.innerHTML = text;
   }
 }
+
+
+window.onload = function() {
+  if (store) {
+    fetchProducts();
+  }
+};
